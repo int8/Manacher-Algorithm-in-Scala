493549459a388fe4cb9a40d8ff80e9a902bbf769
kamil@czarnogorski.pl <kamil@czarnogorski.pl>
2017-11-22 18:35:24 +0100

    removed unused import

--- src/main/scala/solvers/ApproachUsingEERTREE.scala
+++ src/main/scala/solvers/ApproachUsingEERTREE.scala
@@ -1,7 +1,5 @@
 package main.scala.solvers
 
-import scala.collection.immutable.Traversable
-
 abstract class ApproachUsingEERTREE(sentence:String, maxPalindromes:Long) extends Approach(sentence, maxPalindromes) {
   def solve:Int 
   def approachCharacteristics = List("some", "members", "specific", "to", "this", "approach")
