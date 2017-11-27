85b5295de56db48b7e4cf9fd5c264de043636ef4
kamil@czarnogorski.pl <kamil@czarnogorski.pl>
2017-11-27 21:21:55 +0100

    readme

--- src/main/scala/solvers/FunctionalLinearImmutable.scala
+++ src/main/scala/solvers/FunctionalLinearImmutable.scala
@@ -12,7 +12,7 @@ class FunctionalLinearImmutable(sentence: String, maxPalindromes: Long) extends 
     * @param P a Vector with radiuses of previous palindromes found up to P.length (i)in a string
     * @param c a center of the palindrome that reaches current index i
     * @param radius radius of current palindrome ( the one centered at c )
-    * @param i index being currently processed 
+    * @param i index being currently processed
     * @param acc sum of palindromes up to index i
     * */
   @tailrec final def manacherSum(P: Vector[Int], c: Int, radius: Int, i: Int, acc:Int):Int = {
